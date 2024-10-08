89269f1d42ece23f0b2d72efc70e0ec3bad8dd00
chriskiefer <chrisk13fer@gmail.com>
2024-10-08 13:46:29 +0100

    euclid app

---
+++ sw_meml/app_euclidean/src/audio/audio_app.cpp
@@ -0,0 +1,159 @@
+#include "audio_app.h"
+
+#include <vector>
+#include <cmath>
+#include <cstdio>
+
+extern "C" {
+    #include <xcore/channel.h>
+    #include <xcore/channel_streaming.h>
+    #include <xscope.h>
+}
+
+#include "../chans_and_data.h"
+#include "audio_buffers.h"
+// Include audio components
+// #include "FMSynth.hpp"
+
+
+static port_t port1, port2;
+static size_t port1Out=0, port2Out=0;
+
+/**************************
+ * STATIC HELPER FUNCTIONS
+ **************************/
+
+
+template<typename T>
+void clear_buffer(T *buffer, size_t length) {
+    memset(buffer, 0, sizeof(T)*length);
+}
+
+template<typename T>
+void copy_buffer(T *src, T *dst, size_t length) {
+    memcpy(dst, src, sizeof(T)*length);
+}
+
+void to_float_buf(audio_buffer_ptr_t &src, std::vector< std::vector<sample_t> > &dst) {
+
+    static const sample_t scaling = std::pow(2.f, -31);
+
+    for(unsigned int ch = 0; ch < kAudioChannels; ch++) {
+        auto &buffer_ch = dst[ch];
+        for (unsigned int smp = 0; smp < kAudioSamples; smp++) {
+            buffer_ch[smp] = static_cast<sample_t>(src[ch][smp]) * scaling;
+            //if (ch == 0) xscope_float(1, dst[ch][smp]);
+        }
+    }
+}
+
+void from_float_buf(std::vector< std::vector<sample_t> > &src, audio_buffer_ptr_t &dst) {
+    static const sample_t scaling = std::pow(2.f, 31);
+
+    for(unsigned int ch = 0; ch < kAudioChannels; ch++) {
+        const auto &buffer_ch = src[ch];
+        for (unsigned int smp = 0; smp < kAudioSamples; smp++) {
+            dst[ch][smp] = static_cast<int32_t>(buffer_ch[smp] * scaling);
+            //if (ch == 0) xscope_float(3, buffer_ch[smp]);
+        }
+    }
+}
+
+static std::vector< std::vector<sample_t> > sample_buffer;
+
+/**************************
+ * OBJECTS THAT MAKE SOUND
+ **************************/
+
+FMSynth *fmsyn = nullptr;
+static char fmsyn_mem_[sizeof(FMSynth)];
+
+
+/**************************
+ * MAIN ROUTINES
+ **************************/
+
+
+void audio_app_init(float sample_rate, port)_t p1, port_t p2)
+{
+    sample_buffer.resize(kAudioChannels);
+    for (unsigned int ch = 0; ch < kAudioChannels; ch++) {
+        sample_buffer[ch].resize(kAudioSamples, 0);
+    }
+
+    // Component inits
+    // fmsyn = new (fmsyn_mem_) FMSynth(sample_rate);
+    port1 = p1;
+    port2 = p2;
+}
+
+
+static double t=0;
+const size_t nGenerators;
+void audio_loop(chanend_t i2s_audio_in)
+{
+    while (1) {
+        size_t audio_buf_idx = s_chan_in_word(i2s_audio_in);
+        xscope_int(0, 1);
+        audio_buffer_ptr_t audio_buf = audio_buffer_ptrs[audio_buf_idx];
+        to_float_buf(audio_buf, sample_buffer);
+
+        // Floating-point processing here
+
+        for (unsigned int smp = 0; smp < kAudioSamples; smp++) {
+            float y;
+            if (fmsyn != nullptr) {
+                // y = fmsyn->process();
+                for(size_t i_generator=0; i_generator < n_generators; i_generator++) {
+                    bool euclideanSig = 0; //placeholder, get result from a generator
+                    if (i_generator == 7) {
+                        port2Out = euclideanSig;
+                    }else{
+                        if (euclideanSig) {
+                            //on
+                            port1Out |= (0x1 << i_generator);
+                        }else{
+                            //off
+                            port1Out &= (~0x1 << i_generator);
+                        }
+                    }
+                }
+            } else {
+                y = 0;
+            }
+            for(unsigned int ch = 0; ch < kAudioChannels; ch++) {
+                sample_buffer[ch][smp] = y;
+            //if (ch == 0) xscope_float(1, sample_buffer[ch][smp]);
+            }
+            t++;
+        }
+
+
+        // Output
+        from_float_buf(sample_buffer, audio_buf);
+        xscope_int(0, 0);
+    }
+}
+
+
+void audio_app_paramupdate(chanend_t fmsynth_paramupdate)
+{
+    std::vector<num_t> params(kN_synthparams);
+
+    while (true) {
+#if 1
+        chan_in_buf_byte(
+            fmsynth_paramupdate,
+            reinterpret_cast<unsigned char *>(params.data()),
+            sizeof(num_t) * kN_synthparams
+        );
+
+        //debug_printf("FMSynth- Something is received.\n");
+
+        if (fmsyn != nullptr) {
+            fmsyn->mapParameters(params);
+            //std::printf("FMSynth- Params are mapped.\n");
+        }
+#endif
+    }
+}
